4cea6c795bc2fb457e386619eac22299f4787ca2
João Guilherme Sampaio de Sá <joao.sa@hotmail.com.br>
2025-08-08 15:25:20 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,62 @@
-<same as in canvas, omitted here for brevity>
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.30.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_namespace" "infra" {
+  metadata {
+    name = "infra"
+  }
+}
+
+resource "kubernetes_namespace" "apps" {
+  metadata {
+    name = "apps"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  chart      = "prometheus"
+  namespace  = kubernetes_namespace.infra.metadata[0].name
+  repository = "https://prometheus-community.github.io/helm-charts"
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.infra.metadata[0].name
+  repository = "https://grafana.github.io/helm-charts"
+  set {
+    name  = "adminPassword"
+    value = "admin"
+  }
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
+
+resource "helm_release" "myapp" {
+  name       = "myapp"
+  chart      = "./helm/myapp"
+  namespace  = kubernetes_namespace.apps.metadata[0].name
+}
